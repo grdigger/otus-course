eb701629089f5c0a0a4e6f209c5b467a97353054
Sergey Burdachev <svburdachev@avito.ru>
2021-01-21 11:15:13 +0300

    otus course HW1

--- inits/db.go
+++ inits/db.go
@@ -2,12 +2,38 @@ package inits
 
 import (
 	"database/sql"
+	"fmt"
 	_ "github.com/go-sql-driver/mysql"
 	"log"
+	"os"
 )
 
 func NewDB() *sql.DB {
-	db, err := sql.Open("mysql", "mef:derparol@tcp(127.0.0.1:3306)/otus")
+	host := os.Getenv("db_host")
+	port := os.Getenv("db_port")
+	pass := os.Getenv("db_pass")
+	user := os.Getenv("db_user")
+	dbName := os.Getenv("db_name")
+	if len(host) == 0 {
+		log.Fatal("db host is empty")
+	}
+	if len(port) == 0 {
+		log.Fatal("db port is empty")
+	}
+	if len(pass) == 0 {
+		log.Fatal("db pass is empty")
+	}
+	if len(user) == 0 {
+		log.Fatal("db user is empty")
+	}
+	if len(pass) == 0 {
+		log.Fatal("db pass is empty")
+	}
+	if len(dbName) == 0 {
+		log.Fatal("db name is empty")
+	}
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", user, pass, host, port, dbName)
+	db, err := sql.Open("mysql", dsn)
 	if err != nil {
 		log.Fatal("db init error: " + err.Error())
 	}
